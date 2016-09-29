b5f36a837516088848bc2e755552eb9143281cbe
seohanbin <scshb2000@naver.com>
2016-09-29 14:08:02 +0900

    routes and post-controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :bulletins
-  resources :posts
+  resources :bulletins do
+    resources :posts
+  end
+  
   root 'welcome#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
