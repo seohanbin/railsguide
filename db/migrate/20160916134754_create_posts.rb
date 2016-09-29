62713a0f3164021b92c3f29b2289303d71b7e168
seohanbin <scshb2000@naver.com>
2016-09-29 11:33:22 +0900

    migration t.reference added

--- db/migrate/20160916134754_create_posts.rb
+++ db/migrate/20160916134754_create_posts.rb
@@ -3,6 +3,7 @@ class CreatePosts < ActiveRecord::Migration
     create_table :posts do |t|
       t.string :title
       t.text :content
+      t.references :bulletin, index: true, foreign_key: true
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160916134754) do
+ActiveRecord::Schema.define(version: 20160916151651) do
+
+  create_table "bulletins", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer  "bulletin_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
+
+  add_index "posts", ["bulletin_id"], name: "index_posts_on_bulletin_id"
 
 end
